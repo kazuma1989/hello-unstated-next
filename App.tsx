62edadfa03b12cf6f689f6a63d2bde9c978481cc
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-02 18:03:50 +0900

    refactor: Extract App.tsx

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+import { createContainer } from "unstated-next";
+
+function useCounter(initialState = 0) {
+  const [count, setCount] = useState(initialState);
+
+  return {
+    count,
+    decrement() {
+      setCount(v => v - 1);
+    },
+    increment() {
+      setCount(v => v + 1);
+    }
+  };
+}
+
+const Counter = createContainer(useCounter);
+
+function CounterDisplay() {
+  const { count, decrement, increment } = Counter.useContainer();
+
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+export function App() {
+  return (
+    <Counter.Provider>
+      <CounterDisplay />
+
+      <Counter.Provider initialState={1}>
+        <div>
+          <div>
+            <CounterDisplay />
+          </div>
+        </div>
+      </Counter.Provider>
+
+      <Counter.Provider initialState={2}>
+        <CounterDisplay />
+      </Counter.Provider>
+    </Counter.Provider>
+  );
+}
--- index.tsx
+++ index.tsx
@@ -1,53 +1,5 @@
 import ReactDOM from "react-dom";
-import React, { useState } from "react";
-import { createContainer } from "unstated-next";
-
-function useCounter(initialState = 0) {
-  const [count, setCount] = useState(initialState);
-
-  return {
-    count,
-    decrement() {
-      setCount(v => v - 1);
-    },
-    increment() {
-      setCount(v => v + 1);
-    }
-  };
-}
-
-const Counter = createContainer(useCounter);
-
-function CounterDisplay() {
-  const { count, decrement, increment } = Counter.useContainer();
-
-  return (
-    <div>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Counter.Provider>
-      <CounterDisplay />
-
-      <Counter.Provider initialState={1}>
-        <div>
-          <div>
-            <CounterDisplay />
-          </div>
-        </div>
-      </Counter.Provider>
-
-      <Counter.Provider initialState={2}>
-        <CounterDisplay />
-      </Counter.Provider>
-    </Counter.Provider>
-  );
-}
+import React from "react";
+import { App } from "./App";
 
 ReactDOM.render(<App />, document.getElementById("app"));

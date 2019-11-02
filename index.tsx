8eedbb969ac3d7858acdd54145927614e3980eba
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-02 17:58:13 +0900

    feat: Add a basic example

--- index.tsx
+++ index.tsx
@@ -1,8 +1,53 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState } from "react";
+import { createContainer } from "unstated-next";
+
+function useCounter(initialState = 0) {
+  const [count, setCount] = useState(initialState);
+
+  return {
+    count,
+    decrement() {
+      setCount(v => v - 1);
+    },
+    increment() {
+      setCount(v => v + 1);
+    }
+  };
+}
+
+const Counter = createContainer(useCounter);
+
+function CounterDisplay() {
+  const { count, decrement, increment } = Counter.useContainer();
+
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
 
 function App() {
-  return <h1>HELLO</h1>;
+  return (
+    <Counter.Provider>
+      <CounterDisplay />
+
+      <Counter.Provider initialState={1}>
+        <div>
+          <div>
+            <CounterDisplay />
+          </div>
+        </div>
+      </Counter.Provider>
+
+      <Counter.Provider initialState={2}>
+        <CounterDisplay />
+      </Counter.Provider>
+    </Counter.Provider>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
